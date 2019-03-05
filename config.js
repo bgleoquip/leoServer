5d93f956724229f3b3360dd4b1ffbe190084a91e
Bhavesh <bhavesh.gupta@indianic.com>
2019-03-05 15:55:39 +0530

    basic server ready

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    MONGO_URL: "mongodb+srv://bgleoquip:leoquip123@bgcluster-vwvhb.mongodb.net/test?retryWrites=true",
+    MONGO_USERNAME: "bgleoquip",
+    MONGO_PASSWORD: "leoquip123",
+    MONGO_PATH: "",
+    PORT: "8001",
+    HOST: "localhost"
+
+};
+
+module.exports = config;
