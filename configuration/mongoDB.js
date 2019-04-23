1d5abec10e3eb879e72899eb665ea616e1f88a4c
bgbhavesh <bhavesh.gupta@indianic.com>
2019-04-23 16:47:48 +0530

    port to 3000

--- configuration/mongoDB.js
+++ configuration/mongoDB.js
@@ -3,7 +3,7 @@ var config = {
     MONGO_USERNAME: "bgleoquip",
     MONGO_PASSWORD: "leoquip123",
     MONGO_PATH: "",
-    PORT: "8001",
+    PORT: "3000",
     HOST: "localhost",
     dbName: "crudConfig",
     dbUrl: "mongodb://localhost:27017"
