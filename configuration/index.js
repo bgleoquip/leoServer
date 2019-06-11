8ebf3855cc3e2951da56b49566815450b61b1863
unknown <bgleoquip@gmail.com>
2019-06-12 01:57:00 +0530

    sign in about to

--- configuration/index.js
+++ configuration/index.js
@@ -1,5 +1,6 @@
 const { dbName, dbUrl } = require("./mongoDB");
+const { PORT, HOST } = require("./server");
 const { JWT_SECRET, oauth } = require("./jwt");
 module.exports = {
-    dbUrl, dbName, JWT_SECRET, oauth
+    dbUrl, dbName, JWT_SECRET, oauth, PORT, HOST
 }
