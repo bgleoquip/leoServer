5c00103903cd3a71316f007881f613816e5e8844
unknown <bgleoquip@gmail.com>
2019-06-30 20:22:56 +0530

    social login

--- models/user.js
+++ models/user.js
@@ -1,43 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const Schema = mongoose.Schema;
+const schemaData = require("./schema");
 
-// Create a schema
-const userSchema = new Schema({
-  method: {
-    type: String,
-    enum: ['local', 'google', 'facebook'],
-    required: true
-  },
-  local: {
-    email: {
-      type: String,
-      lowercase: true
-    },
-    password: {
-      type: String
-    }
-  },
-  google: {
-    id: {
-      type: String
-    },
-    email: {
-      type: String,
-      lowercase: true
-    }
-  },
-  facebook: {
-    id: {
-      type: String
-    },
-    email: {
-      type: String,
-      lowercase: true
-    }
-  }
-});
-
+let userSchema = schemaData.getUserSchema();
 userSchema.pre('save', async function (next) {
   try {
     console.log('entered');
